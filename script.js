69bc1fab55e0b914eb17b6b7b6c2eed469827886
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-07-25 21:46:20 -0300

    I started the transition to flexbox

---
+++ script.js
@@ -0,0 +1,29 @@
+let valorCarrinho = document.getElementById('valorCarrinho')
+let valorTotal = 0
+let valorUnidade = 10
+
+
+function adicionarAoCarrinho(id) {
+    alert('Item adicionado ao carrinho')
+    
+    let qtdInput = document.getElementById(`qtd${id}`).value
+
+    let quantidade = parseInt(qtdInput)
+    
+    
+    valorTotal += valorUnidade * quantidade
+
+    valorCarrinho.innerHTML = valorTotal
+}
+
+function enviarFormulario() {
+    alert('Dados enviados, em breve entraremos em contato!')
+}
+
+function cadastrar() {
+    alert('E-mail cadastrado com sucesso! Bem vinda(o) a Nails Art Duda!')
+}
+
+function logar() {
+    alert('Login realizado com sucesso! Bem vinda(o) a Nails Art Duda!')
+}
